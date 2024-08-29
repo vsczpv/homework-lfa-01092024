594bb93fdb126c2395e1b9e34abd32b4c5edcee0
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-08-29 17:09:25 -0300

    initial commit

---
+++ src/buscaweb/CapturaRecursosWeb.java
@@ -0,0 +1,56 @@
+/*
+ * Importado de https://github.com/alexrese/BuscaPadraoWeb/
+ */
+
+package buscaweb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+/**
+ * @author Santiago
+ */
+public class CapturaRecursosWeb {
+    private final ArrayList<String> listaRecursos = new ArrayList<>();
+
+
+    public ArrayList<String> carregarRecursos() {
+        ArrayList<String> resultado = new ArrayList<>();
+        for (String stringURL : listaRecursos) {
+            String resposta = "";
+
+            try {
+                URL url = new URL(stringURL);
+                URLConnection connection = url.openConnection();
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                connection.getInputStream()));
+
+                String inputLine;
+
+                StringBuffer sb = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) sb.append(inputLine + "\n");
+                resposta = sb.toString();
+                resultado.add(resposta);
+                in.close();
+            } catch (MalformedURLException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return resultado;
+    }
+
+    /**
+     * @return the listaRecursos
+     */
+    public ArrayList<String> getListaRecursos() {
+        return listaRecursos;
+    }
+}
